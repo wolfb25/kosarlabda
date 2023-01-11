5e0a0532304da5353e3206cd20744c21920b194a
wolfb25 <fbalint0126@gmail.com>
2023-01-11 09:22:21 +0100

    js új fájl, bootstrap kezdete

---
+++ script.js
@@ -0,0 +1,52 @@
+var minutes = 0;
+var seconds = 0;
+var time;
+var hazai_pontok = 0;
+var vendég_pontok = 0;
+
+/*-----------------------*/
+function timedCount() {
+	document.getElementById('szamlalo').value = minutes + ":" + seconds;
+	document.getElementById('negyedelo').value = (((minutes / 15) | 0) + 1) + ". negyed";
+	++seconds;
+	if (seconds % 60 == 0) { ++minutes; seconds = 0; }
+
+	time = setTimeout("timedCount()", 1000);
+}
+
+function doTimer() { timedCount(); }
+function stopTimer() { clearTimeout(time); }
+
+/*--------------------------------------------------*/
+function ajax_get(urlsor, hova, tipus, aszinkron) {
+	$.ajax({
+		url: urlsor, type: "get", async: aszinkron, cache: false, dataType: tipus === 0 ? 'html' : 'json',
+		beforeSend: function (xhr) { },
+		success: function (data) { $(hova).html(data); },
+		error: function (jqXHR, textStatus, errorThrown) { console.log(jqXHR.responseText); },
+		complete: function () { }
+	});
+	return true;
+};
+
+/*-----------------------------------*/
+function ajax_post(urlsor, data, tipus) {     // !! változás: data
+	var s = "";
+	$.ajax({
+		url: urlsor, type: "post", data: data, async: false, cache: false, dataType: tipus === 0 ? 'html' : 'json',
+		beforeSend: function (xhr) { },
+		success: function (data) { s = data; },
+		error: function (jqXHR, textStatus, errorThrown) { console.log(jqXHR.responseText); },
+		complete: function () { }
+	});
+	return s;
+};
+
+/*-----------------------------*/
+$(document).ready(function () {
+
+	var listItems = "";
+	var k_json = ajax_post("kosar.php", "csapatok=1", 1);               // JSON!
+	console.log(k_json);
+
+});
